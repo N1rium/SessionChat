48650c701f1a18b22711c2c3f94c86fe7bbcbfe4
blomman <johnny@gumbler.com>
2017-09-02 13:14:47 +0200

    Initial commit - Base structure

---
+++ dist/js/chat.controller.js
@@ -0,0 +1,176 @@
+angular.module("SocketChat", []).controller("ChatController",
+    ["$scope", "$timeout", function($scope, $timeout) {
+
+    /* Map to hold keys used to communicate with SocketIO */
+    const SOCKET_KEYS = {
+        "ChatMessage"   : "chatmsg",
+        "InputChanged"  : "inputchanged",
+        "UserRename"    : "renameuser",
+    };
+
+    var previousMessages = [];
+
+    let socket;
+
+    function connectIO() {
+        if(socket)
+            return;
+        try {
+            socket = io();
+        } catch(e) {
+
+        }
+    }
+
+    connectIO();
+
+    $scope.chatData = {
+        "self" : {},
+        "messages" : [], /* List of messages */
+        "rooms" : [], /* List of rooms */
+        "users" : [], /* List of users */
+        "typing" : [], /* List of people that are currently typing */
+    };
+
+    function addUser(user) {
+        for(let i = 0; i < $scope.chatData.users.length; i++) {
+            let u = $scope.chatData.users[i];
+            if(u.id == user.id)
+                return;
+        }
+        $scope.chatData.users.push(user);
+    }
+
+    $scope.addRoom = function(room) {
+        for(let i = 0; i < $scope.chatData.rooms.length; i++) {
+            let r = $scope.chatData.rooms[i];
+            if(r == room)
+                return;
+        }
+        $scope.chatData.rooms.push(room);
+    }
+
+    socket.on("chatmsg", function(data) {
+        $scope.$apply(function() {
+            $scope.chatData.messages.push(data);
+            $timeout(function() {
+                $scope.chatScrollBottom();
+            }, 10);
+        });
+    });
+
+    socket.on("welcome", function(data, id) {
+        $scope.$apply(function() {
+            for (var key in data) {
+                addUser(data[key]);
+            }
+        });
+
+        try {
+            localStorage.setItem("socketCache", id);
+            console.log(localStorage);
+        } catch(e) {
+
+        }
+
+        $scope.$apply(function() {
+            $scope.isLogin = true;
+            $scope.chatData.self = self;
+            console.log(self);
+        });
+    });
+
+    socket.on("newuser", function(data) {
+        $scope.$apply(function() {
+            addUser(data);
+        });
+    });
+
+    socket.on("userleft", function(data) {
+        for(var i = 0; i < $scope.chatData.users.length; i++) {
+            var u = $scope.chatData.users[i];
+            if(u.id == data.id) {
+                $scope.$apply(function() {
+                    $scope.chatData.users.splice(i,1);
+                });
+                break;
+            }
+        }
+    });
+
+    socket.on("joinedroom", function(data) {
+        $scope.$apply(function() {
+            $scope.addRoom(data);
+        });
+    });
+
+    socket.on("leftroom", function(data) {
+        for(var i = 0; i < $scope.chatData.rooms.length; i++) {
+            var r = $scope.chatData.rooms[i];
+            if(r == data) {
+                $scope.$apply(function() {
+                    $scope.chatData.rooms.splice(i,1);
+                });
+                break;
+            }
+        }
+    });
+
+    socket.on("cacheduser", function(data) {
+        addUser(data);
+    });
+
+    socket.on("rename", function(data) {
+        for(let i = 0; i < $scope.chatData.messages.length; i++) {
+            let obj = $scope.chatData.messages[i];
+            if(obj["user"].id == data["user"].id) {
+                $scope.$apply(function() {
+                    obj["user"]["username"] = data["name"];
+                });
+            }
+        }
+
+        for(let i = 0; i < $scope.chatData.users.length; i++) {
+            let obj = $scope.chatData.users[i];
+            if(obj.id == data.user.id) {
+                $scope.$apply(function() {
+                    obj.username = data.name;
+                });
+            }
+        }
+    });
+
+    $scope.register = function() {
+        socket.emit("register", $scope.username);
+    }
+
+    $scope.chatScrollBottom = function() {
+        let e = document.getElementById("chatscroll");
+        e.scrollTop = e.scrollHeight;
+    }
+
+    $scope.send = function() {
+        socket.emit(SOCKET_KEYS["ChatMessage"], $scope.chatmsg);
+        previousMessages.push($scope.chatmsg);
+        $scope.chatmsg = "";
+    }
+
+    $scope.inputChange = function(value) {
+        //socket.emit(SOCKET_KEYS["InputChanged"], value);
+    }
+
+    /* Destruction! */
+    $scope.$on("destroy", function() {
+
+    });
+
+    try {
+        var sc = localStorage.getItem("socketCache");
+        console.log("SC: ", sc);
+        if(sc)
+            socket.emit("getcacheduser", sc);
+    } catch(e) {
+
+    }
+
+}]);
---
+++ index.js
@@ -0,0 +1,185 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+/* The prefix that acts as a command */
+const CMD_PREFIX = '/';
+
+/* The room which users automatically joins on login */
+const DEFAULT_ROOM = "Global";
+
+/* List of supported CMDS that will be sent to user upon connection */
+const CMDS = [
+    "rename",
+];
+
+/* How often to do a sweep check for cached users */
+const CLEAR_USER_CACHE_INTERVAL = 1000 * 60 * 5;
+
+/* TTL for how long users can be disconnected until released */
+const USER_CACHE_TTL = 1000 * 60;
+
+/* The main users object */
+let users = {};
+
+/* List of ids cached to remember disconnected users */
+let userCache = [];
+
+/* Internal reference to socket for convenience */
+let _socket;
+
+/* Setup static files catalogue  */
+app.use(require("express").static(__dirname + "/dist"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+/* The main connection stream */
+io.on("connection", function(socket) {
+    _socket = socket;
+
+    function emitClient(client, event, data) {
+        client.rooms.forEach(function(room) {
+            io.to(room).emit(event, data);
+        });
+    }
+
+    function broadcastClient(client, event, data) {
+        client.rooms.forEach(function(room) {
+            io.to(room).broadcast(event, data);
+        });
+    }
+
+    console.log("a user connected");
+
+    // socket.use(function(packet, next) {
+    //     return next();
+    // });
+
+    socket.on("disconnect", function() {
+        if(users[socket.id]) {
+            console.log(users[socket.id].username + " has disconnected");
+            io.emit("userleft", users[socket.id]);
+            userCache.push({
+                "id" : socket.id,
+                "ttl" : Date.now() + USER_CACHE_TTL,
+                "user" : users[socket.id]
+            });
+            delete users[socket.id];
+        }
+    });
+
+    socket.on("chatmsg", function(msg, room = DEFAULT_ROOM) {
+        if(!handleCommands(msg)) {
+            io.to(room).emit("chatmsg", {
+                "message" : msg,
+                "user" : users[socket.id],
+                "date" : new Date(),
+            });
+        }
+    });
+
+    socket.on("istyping", function() {
+        io.broadcast.emit("istyping", users[socket.id].username);
+    });
+
+    socket.on("register", function(name) {
+        newUser(name);
+        welcome(socket);
+    });
+
+    socket.on("getcacheduser", function(id) {
+        userCache.forEach(function(obj) {
+            if(obj.id == id) {
+                console.log("We have a cached user here!");
+                obj.id = socket.id;
+                users[id] = obj.user;
+                //io.to(socket.id).emit("cacheduser", users[id]);
+                welcome(socket);
+            }
+        });
+    });
+
+    socket.on("renameuser", function(name) {
+        renameUser(name);
+    });
+});
+
+/* Checks msg param for command prefix and handles it accordingly */
+function handleCommands(msg) {
+    let id = _socket.id;
+    if(msg.indexOf(CMD_PREFIX) != 0)
+        return false;
+
+    try {
+        let cmd = msg.match("\/[a-zA-Z]+")[0].toLowerCase();
+        let value = msg.split(cmd)[1].trim();
+        let u = users[id];
+
+        if(cmd == "/rename") {
+            renameUser(value);
+            return true;
+        }
+    } catch(e) {
+        return false;
+    }
+
+    return false;
+}
+
+/* Joins default room and welcomes newly connected users */
+function welcome(socket) {
+    joinRoom(DEFAULT_ROOM);
+    io.to(socket.id).emit("welcome", users, socket.id);
+}
+
+/* Basically creates and adds a new user object */
+function newUser(name) {
+    let id = _socket.id;
+    console.log("Creating new user with username: ", name);
+    users[id] = {"id" : id, "username" : name };
+    io.emit("newuser", users[id]);
+}
+
+/* Renames a user */
+function renameUser(name) {
+    let id = _socket.id;
+    let u = users[id];
+    console.log("Renaming user: " + u.username + " with new username: ", name);
+    io.emit("rename", {
+        "name" : name,
+        "user" : u,
+    });
+    u.username = name;
+}
+
+/* Joins room and emits event */
+function joinRoom(room) {
+    _socket.join(room);
+    io.to(room).emit("joinedroom", room);
+}
+
+/* Checks user cache for passed TTLs and clears accordingly */
+function sweepUserCache() {
+    let start = Date.now();
+    let count = 0;
+    for(var i = 0; i < userCache.length; i++) {
+        var u = userCache[i];
+        if(u.ttl < Date.now()) {
+            count++;
+            userCache.splice(i,1);
+            i--;
+        }
+    }
+    console.log("Sweep: ", count, " entries cleared in ", Date.now() - start, "ms");
+}
+
+/* Sweep poll */
+setInterval(function() {
+    sweepUserCache();
+}, CLEAR_USER_CACHE_INTERVAL);
+
+http.listen(3000, function() {
+    console.log("listening on *:3000");
+});
